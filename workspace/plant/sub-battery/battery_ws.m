c63d5f330abcc1d8fe19e36ae4781ca128f4601d
dinamitemic <michelegazzarri@icloud.com>
2021-04-22 22:56:52 +0200

    Model constant parametrization
    -Added matlab files in order two initialize the workspace with all the parameters used in the models
    -Parametrization of resistor, capacitor and thermal coefficient values
    
    TODO: add callback on model launch

---
+++ workspace/plant/sub-battery/battery_ws.m
@@ -0,0 +1,38 @@
+%Cells custom parameters
+CellThermalMass = 30000;
+%CellThermalMass = 527.6215;
+CellSeriesRes = 0.01;
+
+
+%Thermal parameters
+Cell2CellConductiveArea = 1e-2;
+Cell2CellConductiveThickness = 2;
+Cell2CellConductiveThermConduct = 401;
+
+UpperSegmentConductiveArea = 0;
+UpperSegmentConductiveThickness = 0;
+UpperSegmentConductiveThermConduct = 0;
+
+LowerSegmentConductiveArea = 1e-2;
+LowerSegmentConductiveThickness = 2;
+LowerSegmentConductiveThermConduct = 401;
+
+UpperSegment2CaseConductiveArea = 0;
+UpperSegment2CaseConductiveThickness = 0;
+UpperSegment2CaseConductiveThermConduct = 0;
+
+LowerSegment2CaseConductiveArea = 0;
+LowerSegment2CaseConductiveThickness = 0;
+LowerSegment2CaseConductiveThermConduct = 0;
+
+%Balancing circuit parameters
+PassiveBalancingRes1 = 10;
+PassiveBalancingRes2 = 10;
+PassiveBalancingRes3 = 10;
+PassiveBalancingRes4 = 10;
+PassiveBalancingRes5 = 10;
+PassiveBalancingRes6 = 10;
+PassiveBalancingRes7 = 10;
+PassiveBalancingRes8 = 10;
+PassiveBalancingRes9 = 10;
+PassiveBalancingRes10 = 10;
---
+++ workspace/plant/sub-contactor/contactor_ws.m
@@ -0,0 +1,24 @@
+%Contactor Resistance
+ContactorChargerRes = 0.01;
+ContactorInverterRes = 0.01;
+ContactorCommonRes = 0.01;
+
+%Relay Resistance
+RelayPreChargerRes = 0.01;
+RelayPreInverterRes = 0.01;
+
+RelayDisChargerRes = 0.01;
+RelayDisInverterRes = 0.01;
+
+%Pre-charge Resistances
+PreChargerRes = 1000;
+PreInverterRes = 1000;
+%Discharge Resistance
+DisChargerRes = 250;
+DisInverterRes = 250;
+
+%Capacitor
+ChargerCap = 5e-3;
+InverterCap = 2.5e-3;
+
+
